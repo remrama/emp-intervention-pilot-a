46c7bf6a4fa55cc7032c3ca609e983b47aaf1edf
remrama <mallett.remy@gmail.com>
2021-12-04 14:34:28 -0600

    minor

--- plot-empathy_all.py
+++ plot-empathy_all.py
@@ -21,7 +21,6 @@ plt.rcParams["font.sans-serif"] = "Arial"
 
 SAMPLE_RATE = .5
 
-COLORMAP = "Dark2"
 
 import_fname = os.path.join(c.DATA_DIR, "derivatives", "empathy.csv")
 export_fname = os.path.join(c.DATA_DIR, "results", "empathy-all.png")
@@ -30,8 +29,15 @@ export_fname = os.path.join(c.DATA_DIR, "results", "empathy-all.png")
 # load data
 df = pd.read_csv(import_fname)
 
+
+# this whole colormapping is a bit lazy atm
+# but change participant numbers so they aren't so spread out
+# so we can use a categorical colormap
+# (this will change once there are more subjects)
+COLORMAP = "Accent"
 cmap = plt.cm.get_cmap(COLORMAP)
 assert df["participant_id"].nunique() <= cmap.N
+df["participant_id"] = df["participant_id"].map(df["participant_id"].unique().tolist().index)
 
 
 # build the plots
@@ -53,13 +59,13 @@ for (video_id, vid_df), ax in zip(df.groupby("video_id"), axes.flat):
     # plot the actor and crowd nice and bold
     xvals1 = np.arange(0, len(actor_ratings)*SAMPLE_RATE, SAMPLE_RATE)
     xvals2 = np.arange(0, len(crowd_ratings)*SAMPLE_RATE, SAMPLE_RATE)
-    ax.plot(xvals1, actor_ratings, color="k", alpha=1, lw=1, ls="solid", zorder=3)
-    ax.plot(xvals2, crowd_ratings, color="k", alpha=1, lw=1, ls="dashed", zorder=2)
+    ax.plot(xvals1, actor_ratings, label="actor", color="k", alpha=1, lw=1, ls="solid", zorder=3)
+    ax.plot(xvals2, crowd_ratings, label="crowd", color="k", alpha=1, lw=1, ls="dashed", zorder=2)
     ax.text(1, 0, video_id, ha="right", va="bottom", transform=ax.transAxes,
-        fontsize=8)
+        fontsize=6)
     # loop over all the subjects
-    for i, (participant_id, ser) in enumerate(vid_df.groupby("participant_id")["rating"]):
-        color = cmap(i)
+    for participant_id, ser in vid_df.groupby("participant_id")["rating"]:
+        color = cmap(participant_id)
         subj_ratings = stats.zscore(ser.values, nan_policy="raise")
         xvals = np.arange(0, len(subj_ratings)*SAMPLE_RATE, SAMPLE_RATE)
         ax.plot(xvals, subj_ratings, color=color, alpha=.5, lw=1, ls="solid", zorder=1)
@@ -76,6 +82,13 @@ for (video_id, vid_df), ax in zip(df.groupby("video_id"), axes.flat):
     if ax.get_subplotspec().is_first_col() and ax.get_subplotspec().is_last_row():
         ax.set_xlabel("Time (s)")
         ax.set_ylabel("Emotion rating\nz-score")
-
+        legend = ax.legend(bbox_to_anchor=(0, 1), loc="upper left",
+            # title="Rater",
+            fontsize=8,
+            borderaxespad=0, frameon=False,
+            handlelength=1,
+            labelspacing=.2,  # vertical space between entries
+            handletextpad=.2) # space between legend markers and labels
+        # legend._legend_box.align = "left"
 plt.savefig(export_fname)
 plt.close()
